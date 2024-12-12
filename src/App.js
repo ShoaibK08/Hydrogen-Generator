06713e97fdded678a7287f7292d6aec7fe1612e1
Shoaib Khan <shoaibkhan.adt@gmail.com>
2024-12-12 18:30:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Desktop App</h1>
+        <p>Built with React + Electron</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
